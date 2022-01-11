c918330a2c81131482d4afea74bb3c12f1a86323
Kuzia30 <maxzimka37@gmail.com>
2022-01-11 23:28:55 +0200

    add filter

--- src/App.js
+++ src/App.js
@@ -4,9 +4,15 @@ import "./App.css";
 
 class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+    ],
     name: "",
     number: "",
+    filter: "",
   };
 
   handleChange = (evt) => {
@@ -30,6 +36,11 @@ class App extends Component {
   };
 
   render() {
+    const normalazedFilter = this.state.filter.toLowerCase();
+    const visibleContacts = this.state.contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(normalazedFilter)
+    );
+
     return (
       <div>
         <h2>Phonebook</h2>
@@ -60,8 +71,16 @@ class App extends Component {
         </form>
         <div>
           <h2>Contacts</h2>
+          <label htmlFor="contact_filter">Find contacts by name</label>
+          <input
+            onChange={this.handleChange}
+            id="contact_filter"
+            type="text"
+            name="filter"
+            value={this.state.filter}
+          />
           <ul>
-            {this.state.contacts.map((contact) => (
+            {visibleContacts.map((contact) => (
               <li key={contact.id}>
                 {contact.name} : {contact.number}
               </li>
