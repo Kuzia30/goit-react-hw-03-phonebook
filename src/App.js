eaa09d79a7fa2d441c21c55f5b21ccbd1eaa7b1e
Kuzia30 <maxzimka37@gmail.com>
2022-01-11 23:02:00 +0200

    add state number

--- src/App.js
+++ src/App.js
@@ -6,25 +6,27 @@ class App extends Component {
   state = {
     contacts: [],
     name: "",
+    number: "",
   };
 
   handleChange = (evt) => {
-    this.setState({ name: evt.target.value });
+    this.setState({ [evt.target.name]: evt.target.value });
   };
 
   handleSubmit = (evt) => {
     evt.preventDefault();
     this.setState((prevstate) => ({
       contacts: [
-        { name: this.state.name, id: nanoid(5) },
+        { name: this.state.name, number: this.state.number, id: nanoid(5) },
         ...prevstate.contacts,
       ],
     }));
+
     this.reset();
   };
 
   reset = () => {
-    this.setState({ name: "" });
+    this.setState({ name: "", number: "" });
   };
 
   render() {
@@ -42,14 +44,27 @@ class App extends Component {
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
-          />{" "}
+          />
+          <label htmlFor="contact_number">Number</label>
+          <input
+            onChange={this.handleChange}
+            value={this.state.number}
+            id="contact_number"
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
           <button type="submit"> Add contact</button>
         </form>
         <div>
           <h2>Contacts</h2>
           <ul>
             {this.state.contacts.map((contact) => (
-              <li key={contact.id}>{contact.name}</li>
+              <li key={contact.id}>
+                {contact.name} : {contact.number}
+              </li>
             ))}
           </ul>
         </div>
